65b9a3aaa97db5bb810e9aa86148229193ee87c7
michael <mitk13@gmail.com>
2020-09-30 22:17:54 +0300

    model and asign angles

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import mujoco_py as mjc
+
+from joint_names import JointNames
+
 import numpy as np
-
-
 class Model:
 
     def __init__(self, xml_path):
@@ -10,10 +11,14 @@ class Model:
     def visualize(self):
         sim: mjc.MjSim = mjc.MjSim(self.mjc_model)
         viewer: mjc.MjViewer = mjc.MjViewer(sim)
-
+        init_angles = {joint.value:np.pi/2 for joint in JointNames}
+        for name, angle in init_angles.items():
+            print(sim.data.qpos[self.qpos(name)])
+            sim.data.qpos[self.qpos(name)] = sim.data.qpos[self.qpos(name)]+0.1
+        sim.forward()  # Compute forward kinematics
         while True:
-            actions = np.random.uniform(low=-0.001, high=0.001, size=sim.data.ctrl.shape)[:]
-
-            sim.data.ctrl[:] = actions[:]
-            sim.step()
+            # sim.step()
             viewer.render()
+
+    def qpos(self, name):
+        return self.mjc_model.get_joint_qpos_addr(name)
