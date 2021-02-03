990f8c47bc071d094e6158129b72742998f075c9
michael <mitk13@gmail.com>
2021-02-03 22:55:49 +0200

    rotate but not leg fix

--- adaptive_leveling_main.py
+++ adaptive_leveling_main.py
@@ -1,12 +1,15 @@
+import matplotlib.pyplot as plt
 import os
 
 import numpy as np
 from scipy.spatial.transform import Rotation
 from tqdm import tqdm
 
+from kinematics.ik_algorithm import angles_to_target
 from kinematics.joint_kinematics import KinematicNumericImpl
 from model.hexapod_env import HexapodEnv
 from model.joint_types import JointNames
+from model.leg import all_legs
 from neuro.plane_angles import PlaneRotation
 
 '''
@@ -36,10 +39,10 @@ prevp1 = prevp2 = np.zeros(3)
 state = env.qpos
 for v in qpos_map.values():
     state[v] = 0
+
 env.set_state(state, np.zeros_like(env.qvel))
-# history = []
 new_pos = state
-for _ in tqdm(range(1000)):
+for _ in tqdm(range(500)):
     p1 = env.get_pos(joint1)
     p2 = env.get_pos(joint2)
     idxs = [1, 2]  # x,z axis
@@ -47,14 +50,26 @@ for _ in tqdm(range(1000)):
                        (p2[idxs] - prevp2[idxs]))
     prevp1, prevp2 = p1.copy(), p2.copy()
 
+    # rotate
     rot_angle = neuro_model.curr_val
     new_pos = env.qpos
     rot = Rotation.from_euler('xyz', [0, -rot_angle, 0], degrees=True)
     x, y, z, w = rot.as_quat()
     new_pos[3:7] = [w, x, y, z]
-
-    env.step(new_pos, render=True)
-
+    env.set_state(new_pos, 0 * env.qvel)
+    env.render()
+    # adjust position
+    # adjustment_pos = env.qpos
+    # adjustment_pos[0:3] = np.zeros(3)
+    # for leg in all_legs:
+    #     coxa = qpos_map[leg.coxa.value]
+    #     femur = qpos_map[leg.femur.value]
+    #     tibia = qpos_map[leg.tibia.value]
+    #     joint_pos = [coxa, femur, tibia]
+    #     adjustment_pos[joint_pos], e = angles_to_target(q=adjustment_pos[joint_pos], target=-leg.rotate(diff))
+    #
+    # env.set_state(adjustment_pos, 0 * env.qvel)
+    # env.render()
 env.close()
 
 # x, y = neuro_model.get_xy()
