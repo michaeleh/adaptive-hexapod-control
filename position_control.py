835b2e099044007edc5e25fb47e246c27131e7e3
michael <mitk13@gmail.com>
2021-03-17 17:02:49 +0200

    presice model

--- position_control.py
+++ position_control.py
@@ -3,15 +3,19 @@ from mujoco_py import load_model_from_path, MjSim, MjViewer
 import os
 
 model = load_model_from_path(
-    "/home/michael/University/Master/Thesis/adaptive-hexapod-control/mujoco-models/mk3/mk3.xml")
+    "/home/michael/University/Master/Thesis/adaptive-hexapod-control/mujoco-models/hexapod/hexapod.xml")
 sim = MjSim(model)
 
 viewer = MjViewer(sim)
 
 sim_state = sim.get_state()
-
+body_names = list(model.body_names)
+i = 0
 while True:
+    i += 1
     sim.data.ctrl[:] = np.deg2rad(0)
-    sim.data.ctrl[1] = np.deg2rad(50)
+    if i > 1000:
+        sim.data.ctrl[1] = np.deg2rad(-40)
+
     sim.step()
     viewer.render()
